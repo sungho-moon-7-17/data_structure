995207e9dba6a7944cff21d45985660673cc4c53
sungho8203 <sungho8203@gmail.com>
2021-11-01 22:30:05 +0900

    normal_list modify

--- linked_list/listFuntion.c
+++ linked_list/listFuntion.c
@@ -2,49 +2,46 @@
 
 void ListInit(t_list *plist)
 {
-    plist->curPosition = 0;
-    plist->numOfData = 0;
+	plist->curPosition = 0;
+	plist->numOfData = 0;
 }
 
 void LInsert(t_list *plist, LData data)
 {
-    plist->arr[plist->curPosition] = data;
-    plist->numOfData++;
-    plist->curPosition++;
+	plist->arr[plist->curPosition] = data;
+	plist->numOfData++;
+	plist->curPosition++;
 }
 
-int LFirst(t_list *plist, LData * pdata)
+int LFirst(t_list *plist, LData *data)
 {
-    plist->curPosition = 0;
-    if (plist->numOfData <= 0)
-        return (FALSE);
-    *pdata = plist->arr[plist->curPosition];
-    return (TRUE);
-}
-
-int LNext(t_list *plist, LData *pdata)
-{
-    plist->curPosition++;
-	if (LIST_LEN < plist->numOfData || plist->curPosition > (plist->numOfData - 1))
+	if (plist->numOfData <= 0)
 		return (FALSE);
-    *pdata = plist->arr[plist->curPosition];
-    return (TRUE);
+	plist->curPosition = 0;
+	*data = plist->arr[0];
+	return (TRUE);
+}
+
+int LNext(t_list *plist, LData *data)
+{
+	plist->curPosition++;
+	if ((plist->curPosition > plist->numOfData - 1) || (plist->numOfData >= 100))
+		return (FALSE);
+	*data = plist->arr[plist->curPosition];
+	return (TRUE);
 }
 
 LData LRemove(t_list *plist)
 {
-	LData temp = plist->arr[plist->curPosition];
-    for (int i = plist->curPosition; i < plist->numOfData - 1; i+
-	+)
-    {
-        plist->arr[i] = plist->arr[i + 1];
-    }
+	LData return_value =  plist->arr[plist->curPosition];
 	plist->numOfData--;
+	for(int i = plist->curPosition; i < plist->numOfData; i++)
+		plist->arr[i] = plist->arr[i + 1];
 	plist->curPosition--;
-	return (temp);
+	return (return_value);
 }
 
 int LCount(t_list *plist)
 {
-    return plist->numOfData;
+	return (plist->numOfData);
 }
